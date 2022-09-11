f52e0d5bd174df5d87d49d30e8dbcac167fd0d30
EnriqueRS <enrique.rios.santos@gmail.com>
2022-09-11 15:47:00 +0200

    eslint added

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,19 +1,18 @@
-import React, { useEffect } from 'react';
-import sendGetRequest from '../../api/sendGetRequest';
-import UseToken from '../../middlewares/useToken';
+import React, { useEffect } from 'react'
+import sendGetRequest from '../../api/sendGetRequest'
+import UseToken from '../../middlewares/useToken'
 
+function Dashboard (props) {
+  const { token } = UseToken()
+  useEffect(() => {
+    sendGetRequest('todo', token)
+  })
 
-function Dashboard(props) {
-    const { token } = UseToken();
-    useEffect(() => {
-        sendGetRequest('todo', token);
-    }, [])
-
-    return (
-        <div id='container'>
-            <h1>Dashboard Client</h1>
-        </div>
-    );
+  return (
+    <div id='container'>
+        <h1>Dashboard Client</h1>
+    </div>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
