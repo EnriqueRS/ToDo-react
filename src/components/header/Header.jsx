419ae07da4a30f93addf4e16f09601dbaf28fd68
EnriqueRS <enrique.rios.santos@gmail.com>
2022-09-18 18:42:00 +0200

    header component renamed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './header.module.css'
 
 function Header () {
   return (
     <header>
         <div>ToDo App</div>
-        <ul className='menu'>
+        <ul className={styles.menu}>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/private'>Private</Link></li>
         </ul>
