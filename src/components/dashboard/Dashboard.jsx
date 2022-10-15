63d0031b8f5d9d5c4950176f2bb104941c0e03e5
EnriqueRS <enrique.rios.santos@gmail.com>
2022-10-15 18:04:00 +0200

    show new dashboard with draggable option

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ import sendGetRequest from '../../api/sendGetRequest'
 import UseToken from '../../middlewares/useToken'
 import { FaTasks } from 'react-icons/fa'
 import styles from './dashboard.module.css'
-import ToDo from '../todo/ToDo'
+import Column from '../column/Column'
 
 function Dashboard (props) {
   const { token } = UseToken()
@@ -13,40 +13,24 @@ function Dashboard (props) {
 
   return (
     <div className={styles.container}>
-        <div className={styles.sidebar}>
-          <h2>ToDo APP</h2>
-          <FaTasks className={`${styles.logo} ${styles.icon_white}`} />
+      <div className={styles.sidebar}>
+        <h2>ToDo APP</h2>
+        <FaTasks className={`${styles.logo} ${styles.icon_white}`} />
+      </div>
+      <div className={styles.main}>
+        <div className={`${styles.todo} ${styles.column}`}>
+          <Column type={'todo'}
+            todos={['sport', 'home', 'other', 'development', 'unknown', 'language']} />
         </div>
-        <div className={styles.main}>
-          <div className={`${styles.todo} ${styles.column}`}>
-            <div className='column-header'>
-              <h2>To Do</h2>
-            </div>
-            <div className={styles.cards}>
-              <ToDo category={'sport'} />
-              <ToDo category={'development'} />
-              <ToDo category={'language'} />
-            </div>
-          </div>
-          <div className={`${styles.inprogress} ${styles.column}`}>
-            <div className='column-header'>
-              <h2>In Progress</h2>
-            </div>
-            <div className={styles.cards}>
-              <ToDo category={'sport'} />
-              <ToDo category={'development'} />
-            </div>
-          </div>
-          <div className={`${styles.done} ${styles.column}`}>
-            <div className='column-header'>
-              <h2>Done</h2>
-            </div>
-            <div className={styles.cards}>
-              <ToDo category={'home'} />
-              <ToDo category={'other'} />
-            </div>
-          </div>
+        <div className={`${styles.inprogress} ${styles.column}`}>
+          <Column type={'doing'}
+            todos={['sport', 'home', 'other', 'development']} />
         </div>
+        <div className={`${styles.done} ${styles.column}`}>
+          <Column type={'done'}
+            todos={['sport', 'home', 'unknown', 'language']} />
+        </div>
+      </div>
     </div>
   )
 }
