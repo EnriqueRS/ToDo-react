5b3a1c78e15bee9876acca92a48e5d644054afe2
EnriqueRS <enrique.rios.santos@gmail.com>
2022-10-29 16:07:00 +0200

    add Main component

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './main.module.css'
+
+function Main (props) {
+  return (
+        <div className={`${styles.container}`}>
+        </div>
+  )
+}
+
+export default Main
