cf83453cd072ba24f765880863466417832bbd90
EnriqueRS <enrique.rios.santos@gmail.com>
2022-11-01 20:41:00 +0100

    Sidebar added in dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import sendGetRequest from '../../api/sendGetRequest'
 import UseToken from '../../middlewares/useToken'
-import { FaTasks } from 'react-icons/fa'
 import styles from './dashboard.module.css'
-import Column from '../column/Column'
+import Sidebar from '../sidebar/Sidebar'
+import Main from '../main/Main.jsx'
 
 function Dashboard (props) {
   const { token } = UseToken()
-  const [todos, setTodos] = useState([])
-  const [doing, setDoing] = useState([])
-  const [done, setDone] = useState([])
+  const [tags, setTags] = useState({})
 
   useEffect(() => {
     sendGetRequest('todo', token)
       .then((response) => {
-        setTodos(response.filter((item) => item.state === 'todo'))
-        setDoing(response.filter((item) => item.state === 'doing'))
-        setDone(response.filter((item) => item.state === 'done'))
+        const arrayTags = response.map((item) => item.type)
+        const res = arrayTags.reduce(function (types, current) {
+          types[current] = (types[current] || 0) + 1
+          return types
+        }, {})
+        setTags(res)
       }).catch((error) => {
         console.log(error)
       })
@@ -25,23 +26,9 @@ function Dashboard (props) {
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
-        <h2>ToDo APP</h2>
-        <FaTasks className={`${styles.logo} ${styles.icon_white}`} />
+        <Sidebar tags={tags} />
       </div>
-      <div className={styles.main}>
-        <div className={`${styles.todo} ${styles.column}`}>
-          <Column type={'todo'}
-            items={todos} />
-        </div>
-        <div className={`${styles.inprogress} ${styles.column}`}>
-          <Column type={'doing'}
-            items={doing} />
-        </div>
-        <div className={`${styles.done} ${styles.column}`}>
-          <Column type={'done'}
-            items={done} />
-        </div>
-      </div>
+      <Main />
     </div>
   )
 }
