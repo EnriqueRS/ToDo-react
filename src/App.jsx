6b99d017f126b2b4cc4ff0c9081de803debdc1ba
EnriqueRS <enrique.rios.santos@gmail.com>
2022-11-06 15:58:00 +0100

    dashboard is now private

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,29 @@ import React from 'react'
 import './App.css'
 import Login from './components/login/Login'
 import Dashboard from './components/dashboard/Dashboard'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import useToken from './middlewares/useToken'
+import { Routes, Route } from 'react-router-dom'
+import PrivateRoute from './middlewares/PrivateRoute'
 
 function App () {
-  const { setToken } = useToken()
-
   return (
     <div className='App'>
       <div className='page'>
         <div className='content'>
-          <BrowserRouter>
-            <Routes>
-              <Route path='/' exact={true} element={<Login onSaveToken={setToken} />} />
-              <Route path='/dashboard' exact={true} element={<Dashboard />} />
-            </Routes>
-          </BrowserRouter>
-        </div>
-      </div>
-    </div>
+          <Routes>
+            <Route path='/' exact={true} element={<Login />} />
+            <Route
+              path='/dashboard'
+              exact={true}
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+          </Routes >
+        </div >
+      </div >
+    </div >
   )
 }
 
