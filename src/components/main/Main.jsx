868a3605e3b0e4568e4b4e9eba9f8c892ff0eabf
EnriqueRS <enrique.rios.santos@gmail.com>
2022-11-27 18:14:00 +0100

    add main component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import styles from './main.module.css'
+import ToDo from '../todo/ToDo'
+import PropTypes from 'prop-types'
 
 function Main (props) {
+  const todoArray = props.todos
+
   return (
-        <div className={`${styles.container}`}>
-        </div>
+    <div className={`${styles.container}`}>
+      <div className={styles.editText} contentEditable='true' >
+
+      </div>
+      <div className={styles.todos}>
+        {
+          Array.from(todoArray).map((item) => (
+            <ToDo key={item.id}
+              id={item.id}
+              category={item.type}
+              title={item.title}
+              state={item.state} />
+          ))
+        }
+      </div>
+    </div>
   )
+}
+
+Main.propTypes = {
+  todos: PropTypes.array
 }
 
 export default Main
