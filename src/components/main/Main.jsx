7f0bafc1e675bf6b5693c1ed7b50bb2c22dd57c4
EnriqueRS <enrique.rios.santos@gmail.com>
2023-01-07 16:23:00 +0100

    add categories when new todo added

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './main.module.css'
 import ToDo from '../todo/ToDo'
 import PropTypes from 'prop-types'
+import Tag from '../tag/Tag'
 
 function Main (props) {
   const todoArray = props.todos
+  const types = [...new Set(todoArray.map((item) => item.type))]
+  const [newTodo, setNewTodo] = useState('')
+  const [showCategories, setShowCAtegories] = useState(false)
+
+  const handleChange = (event) => {
+    setShowCAtegories(event.target.value !== '')
+    setNewTodo(event.target.value)
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      setNewTodo(newTodo)
+    }
+  }
 
   return (
     <div className={`${styles.container}`}>
-      <div className={styles.editText} contentEditable='true' >
+      <input
+        className={styles.editText}
+        type="text"
+        id="newTodo"
+        name="newTodo"
+        placeholder="Add new todo"
+        value={newTodo}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
 
-      </div>
+      {showCategories && <div className={styles.categories}>
+        {types.map(function (keyName, keyIndex) {
+          return (
+            <Tag key={keyIndex} name={keyName} number={undefined} />
+          )
+        })}
+      </div>}
+
       <div className={styles.todos}>
         {
           Array.from(todoArray).map((item) => (
